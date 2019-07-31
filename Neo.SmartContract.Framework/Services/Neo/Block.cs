6e379d2116680c1725e03dd234c45fac70236273
pathhive <44360241+pathhive@users.noreply.github.com>
2019-07-31 16:19:18 +0900

    Add files via upload

---
+++ Neo.SmartContract.Framework/Services/Neo/Block.cs
@@ -0,0 +1,14 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public class Block : Header
+    {
+        [Syscall("System.Block.GetTransactionCount")]
+        public extern int GetTransactionCount();
+
+        [Syscall("System.Block.GetTransactions")]
+        public extern Transaction[] GetTransactions();
+
+        [Syscall("System.Block.GetTransaction")]
+        public extern Transaction GetTransaction(int index);
+    }
+}
---
+++ Neo.SmartContract.Framework/Services/Neo/StorageContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public class StorageContext
+    {
+        [Syscall("System.StorageContext.AsReadOnly")]
+        public static extern void AsReadOnly(StorageContext context);
+    }
+}
